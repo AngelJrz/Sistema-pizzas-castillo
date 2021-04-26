d4f4c27d7e5dd23d58feeaca31d44a90032b2b19
Rick <51493230+Frews2@users.noreply.github.com>
2021-04-25 21:42:19 -0500

    Desarrollo de Primeras Versiones de GUI de ModuloProductos
    Creación de RegistroDeProducto.xaml, ActualizaciónDeProducto.xaml,DetallesDeProducto.xaml, Inicio-Gerente-Producto.xaml, y GeneraciónDeReporteInventario.xaml

---
+++ PizzasCastillo/Presentacion/Paginas/Producto/Inicio-Gerente-Productos.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Presentacion.Paginas.Producto
+{
+    /// <summary>
+    /// Interaction logic for Inicio_Gerente_Productos.xaml
+    /// </summary>
+    public partial class Inicio_Gerente_Productos : Page
+    {
+        public Inicio_Gerente_Productos()
+        {
+            InitializeComponent();
+        }
+    }
+}
