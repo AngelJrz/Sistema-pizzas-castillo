892e9890d358835370170972354a781e2a52095b
Frews2 <parkersjames87@gmail.com>
2021-05-04 19:01:44 -0500

    Creación de  Clone en Merma.cs

--- PizzasCastillo/Dominio/Entidades/Merma.cs
+++ PizzasCastillo/Dominio/Entidades/Merma.cs
@@ -13,5 +13,16 @@ namespace Dominio.Entidades
         public string Justificacion { get; set; }
         public List<Indica> Indica { get; set; }
         public Pedido Pedido { get; set; }
+        public static Merma Clone(AccesoADatos.Merma merma)
+        {
+            return new Merma
+            {
+                Id = merma.Id,
+                Fecha = merma.Fecha,
+                Justificacion = merma.Justificacion,
+                Indica  = Entidades.Indica.CloneList(merma.Indica.ToList()),
+                Pedido = Pedido.Clone(merma.Pedido)
+            };
+        }
     }
 }
