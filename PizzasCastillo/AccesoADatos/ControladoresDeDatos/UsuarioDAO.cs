dc9c95fa1b38db532e38e18713fdb428abdd6ae9
SammyGCh <53235299+SammyGCh@users.noreply.github.com>
2021-05-05 23:12:23 -0500

    creación clase Singleton y UsuarioDAO

---
+++ PizzasCastillo/AccesoADatos/ControladoresDeDatos/UsuarioDAO.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoADatos.ControladoresDeDatos
+{
+    public class UsuarioDAO
+    {
+        private PizzasCastilloBDConnection _connection;
+        private List<Persona> _empleados;
+        private const int ACTIVO = 1;
+        private const int NINGUN_CAMBIO_REALIZADO = 0;
+        private int _resultado;
+
+        public UsuarioDAO()
+        {
+            _connection = new PizzasCastilloBDConnection();
+            _resultado = 0;
+        }
+
+        public List<Persona> ObtenerEmpleadosActivos()
+        {
+            _empleados = _connection.Persona
+                .Where(persona => persona.Estatus == ACTIVO)
+                .Include("Empleado")
+                .ToList();
+
+            return _empleados;
+        }
+
+        public bool RegistrarEmpleado(Persona empleado)
+        {
+            _connection.Entry(empleado).State = EntityState.Added;
+
+            try
+            {
+                _resultado = _connection.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+
+                throw;
+            }
+
+            if (_resultado == NINGUN_CAMBIO_REALIZADO)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ PizzasCastillo/Presentacion/Recursos/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion.Recursos
+{
+    public class Singleton
+    {
+        private static Singleton _instancia;
+        public HashSet<object> Recursos { get; set; }
+
+        private Singleton() 
+        {
+            Recursos = new HashSet<object>();
+        }
+
+        public static Singleton ObtenerInstancia()
+        {
+            if (_instancia == null)
+            {
+                _instancia = new Singleton();
+            }
+
+            return _instancia;
+        }
+    }
+}
