73f94465058270b97e518407c51964383c8edc35
jonathan <jhoni65475@gmail.com>
2021-05-15 13:30:12 -0500

    cambios de bd

--- PizzasCastillo/AccesoADatos/PizzasCastillo.Designer.cs
+++ PizzasCastillo/AccesoADatos/PizzasCastillo.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'D:\ProyectosVisualStudio\Sistema-pizzas-castillo\PizzasCastillo\AccesoADatos\PizzasCastillo.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\jhoni\Documents\8vo semestre\Sistema-pizzas-castillo\PizzasCastillo\AccesoADatos\PizzasCastillo.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
